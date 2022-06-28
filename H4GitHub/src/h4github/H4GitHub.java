b93bc02efe7488ad42ba1d9dae74e09147b5de11
fernando <fernando@192.168.1.63>
2022-06-28 08:26:09 -0500

    primera modificacion

--- H4GitHub/src/h4github/H4GitHub.java
+++ H4GitHub/src/h4github/H4GitHub.java
@@ -16,6 +16,13 @@ public class H4GitHub {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        
+        
+    int x = 4;
+    int y= 5;
+    int suma= x+y;
+    System.out.println("Suma:"+suma);
     }
     
 }
